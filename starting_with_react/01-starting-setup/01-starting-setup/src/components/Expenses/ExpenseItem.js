01bb5ef085000515d848ed6afa66cee54272d145
sarvesh-7 <sarveshgawade42@gmail.com>
2022-10-14 18:28:22 +0530

    learnt event handling

--- starting_with_react/01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ starting_with_react/01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,10 +4,14 @@ import ExpenseDetails from './ExpenseDetails.js';
 import Card from '../UI/card.js';
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log("clicked");
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <ExpenseDetails title={props.title} amount={props.amount}/>
+            <button onClick={clickHandler}>Delete</button>
         </Card>
     );
 };
