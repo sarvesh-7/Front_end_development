8d41e2a9f4064e45362aba25400fc8d772bff6da
sarvesh-7 <sarveshgawade42@gmail.com>
2022-10-14 19:58:24 +0530

    learnt how to use State

--- starting_with_react/01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ starting_with_react/01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,24 @@
+import React, {useState} from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate.js';
 import ExpenseDetails from './ExpenseDetails.js';
 import Card from '../UI/card.js';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+    const [expAmt, setExpense] = useState(props.amount);
     const clickHandler = () => {
-        console.log("clicked");
+        setTitle("Updated");
+    }
+    const changeExpense = ()=>{
+        setExpense("100");
     }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
-            <ExpenseDetails title={props.title} amount={props.amount}/>
-            <button onClick={clickHandler}>Delete</button>
+            <ExpenseDetails title={title} amount={expAmt}/>
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={changeExpense}>Change Expense</button>
         </Card>
     );
 };
