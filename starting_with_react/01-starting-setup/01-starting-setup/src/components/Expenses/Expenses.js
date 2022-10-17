557ee8ebfc959a39bd27d1834f6724a657c09f5a
sarvesh-7 <sarveshgawade42@gmail.com>
2022-10-17 17:37:47 +0530

    added filter componentfor year

--- starting_with_react/01-starting-setup/01-starting-setup/src/components/Expenses/Expenses.js
+++ starting_with_react/01-starting-setup/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/card.js';
+import ExpensesFilter from './ExpenseFilter';
+import {useState} from 'react';
 const Expenses = (props) => {
     let expCompArr = [];
+    const [year, setYear] = useState('2019');
+
+    const changeYear = year =>{
+        setYear(year);
+        console.log(year);
+    }
     for (let i = 0; i < props.items.length; i++) {
         expCompArr.push (
-            <ExpenseItem title={
+            <ExpenseItem key={
+                props.items[i].id
+            }
+            title={
                 props.items[i].title
                 }
                 amount={
@@ -18,9 +29,12 @@ const Expenses = (props) => {
         );
     };
     return(
+        <div>
         <Card className="expenses">
+        <ExpensesFilter selected = {year} onChangeYear = {changeYear} />
             {expCompArr}
         </Card>
+        </div>
         
     );
 }
