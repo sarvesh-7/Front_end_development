16fde8bec2f1fc34bd17d13d3b229494ff33319a
sarvesh-7 <sarveshgawade42@gmail.com>
2022-10-20 19:12:30 +0530

    removed extra unnecessary buttons

--- starting_with_react/01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ starting_with_react/01-starting-setup/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -7,19 +7,11 @@ import Card from '../UI/card.js';
 const ExpenseItem = (props) => {
     const [title, setTitle] = useState(props.title);
     const [expAmt, setExpense] = useState(props.amount);
-    const clickHandler = () => {
-        setTitle("Updated");
-    }
-    const changeExpense = ()=>{
-        setExpense("100");
-    }
     return (
         <li>
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <ExpenseDetails title={title} amount={expAmt}/>
-            <button onClick={clickHandler}>Change Title</button>
-            <button onClick={changeExpense}>Change Expense</button>
         </Card>
         </li>
     );
