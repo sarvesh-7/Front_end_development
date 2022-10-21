8431db739014363c87defe6cfada16c55b1449a7
sarvesh-7 <sarveshgawade42@gmail.com>
2022-10-21 16:51:44 +0530

    shifted course goal project files to new react project

---
+++ React_module_3/course_goals_project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  //state to check if value entered by user is valid or invalid
+  const[isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = event => {
+    setIsValid(true);
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+      //trim - remove extra spaces as blank spaces are invalid as well
+      if(enteredValue.trim().length === 0){
+        setIsValid(false);
+        return;
+      }
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button isValidState = {isValid} type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
