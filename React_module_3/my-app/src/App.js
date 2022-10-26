da52a0c43de7ae2a6dda771dba949d7cf1013614
sarvesh-7 <sarveshgawade42@gmail.com>
2022-10-26 14:58:58 +0530

    worked with context API

--- React_module_3/my-app/src/App.js
+++ React_module_3/my-app/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from './Components/Login/Login';
 import Home from './Components/Home/Home';
 import MainHeader from './Components/MainHeader/MainHeader';
+import AuthContext from './Components/store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -29,13 +30,16 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <AuthContext.Provider value = {{
+        isLoggedIn : isLoggedIn,
+        onLogout : logoutHandler
+    }}>
+      <MainHeader />
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!isLoggedIn && <Login/>}
+        {isLoggedIn && <Home />}
       </main>
-    </React.Fragment>
+      </AuthContext.Provider>
   );
 }
 
