33c76475a1629beebf75bfd58745b65e9985b33a
sarvesh-7 <sarveshgawade42@gmail.com>
2022-11-13 20:46:53 +0530

    Added error handling logic

--- React_module_3/movies-app/src/App.js
+++ React_module_3/movies-app/src/App.js
@@ -1,52 +1,68 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const[isLoading, setIsLoading] = useState(false);
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    let id;
 
- const fetchMoviesHandler = async()=>{
+    const fetchMoviesHandler = async () => { // update state is loading
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch('https://swapi.dev/api/films/');
+            if (!response.ok) {
+                throw new Error('Something went wrong.. Retrying');
+            }
+            const data = await response.json();
 
-  //update state is loading
-  setIsLoading(true);
-   const response = await fetch('https://swapi.dev/api/films/')
-   const data = await response.json();
+            const transformedMovieData = data.results.map((movieData) => {
+                return {id: movieData.episode_id, title: movieData.title, openingText: movieData.opening_crawl, releaseDate: movieData.release_data};
+            });
+            setMovies(transformedMovieData);
+        } catch (error) {
+          console.log('I am called');
+            setError(error.message);
+        }
+        // set isLoading to false once received data from back end
+        setIsLoading(false);
+    }
 
-     const transformedMovieData = data.results.map((movieData)=>{
-       return {
-         id : movieData.episode_id,
-         title : movieData.title,
-         openingText : movieData.opening_crawl,
-         releaseDate : movieData.release_data
-       };
-     });
-    setMovies(transformedMovieData);
+    if(error){
+      id = setTimeout(fetchMoviesHandler, 5000);
+    }
 
-    //set isLoading to false once received data from back end
-   setIsLoading(false);
-   }
+    const stopRetryingHanlder = ()=>{
+      console.log("I am cleared");
+      clearTimeout(id);
+      setError(null);
+    }
 
-  return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      {isLoading && 
-      <section>
-        <h2>Data is Loading...</h2>
-      </section>
-      }
-      {
-        !isLoading && movies.length > 0&&
-        <section>
-        <MoviesList movies={movies} />
-      </section>
-      }
-      
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+                <button onClick={stopRetryingHanlder}>Stop Retrying</button>
+            </section>
+            {
+            isLoading && <section>
+                <h2>Data is Loading...</h2>
+            </section>
+        }
+            {
+            !isLoading && error && <section>
+                <p>{error}</p>
+            </section>
+        }
+            {
+            !isLoading && movies.length > 0 && <section>
+                <MoviesList movies={movies}/>
+            </section>
+        } </React.Fragment>
+    );
 }
 
 export default App;
