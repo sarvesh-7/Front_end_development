8706c282e5982d857927bd2b7af8f0836a02c640
sarvesh-7 <sarveshgawade42@gmail.com>
2022-11-14 16:39:36 +0530

    added movie add/delet functionality

--- React_module_3/movies-app/src/App.js
+++ React_module_3/movies-app/src/App.js
@@ -1,5 +1,5 @@
-import React, {useState,useEffect} from 'react';
-
+import React, {useState,useEffect,useCallback} from 'react';
+import Button from './components/UI/Button';
 import MoviesList from './components/MoviesList';
 import AddMoviesForm from './components/AddMoviesForm';
 import './App.css';
@@ -9,52 +9,124 @@ function App() {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
     let id;
+    let loadedMovieData = [];
 
-    const fetchMoviesHandler = async () => { // update state is loading
+    const fetchMoviesHandler = useCallback(async () => { // update state isLoading and clear error state
         setIsLoading(true);
         setError(null);
+        //fetch movie data from firebase
         try {
-            const response = await fetch('https://swapi.dev/api/films/');
+            const response = await fetch('https://movies-app-bc878-default-rtdb.firebaseio.com/Movies.json');
             if (!response.ok) {
                 throw new Error('Something went wrong.. Retrying');
             }
-            const data = await response.json();
+            const data = await response.json(); //convert movie data from JSON to javascript object
 
-            const transformedMovieData = data.results.map((movieData) => {
-                return {id: movieData.episode_id, title: movieData.title, openingText: movieData.opening_crawl, releaseDate: movieData.release_data};
-            });
-            setMovies(transformedMovieData);
+            loadedMovieData = []; //set initial loaded data as empty
+
+            //load all movie data fetched from API to an array
+            for(const key in data){
+                loadedMovieData.push({
+                    id : key,
+                    title : data[key].title,
+                    openingText : data[key].openingText,
+                    releaseDate : data[key].releaseDate
+                });
+            }
+            //update movie data in state to render on screen
+            setMovies(loadedMovieData);
+
         } catch (error) {
-          console.log('I am called');
             setError(error.message);
         }
         // set isLoading to false once received data from back end
         setIsLoading(false);
-    }
+    },[]);
 
+    //if there is an error make request to the API repeatedly in every 5 sec
     if(error){
       id = setTimeout(fetchMoviesHandler, 5000);
     }
 
-    const stopRetryingHanlder = ()=>{
+    //stop retrying again and again once clicked on stop retrying button
+    const stopRetryingHanlder = useCallback(()=>{
       console.log("I am cleared");
       clearTimeout(id);
       setError(null);
-    }
+    },[id]);
 
+    //fetch all movie data when dom content loaded
     useEffect(()=>{
         console.log('API call made in useEffect');
         fetchMoviesHandler()
     },[]);
 
+    //add movie data to the backend
+    const addMovies = useCallback(async (movie)=>{
+        try
+        {
+            const response  = await fetch('https://movies-app-bc878-default-rtdb.firebaseio.com/Movies.json',
+        {
+            method : 'POST',
+            body : JSON.stringify(movie),
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+        });
+        const data = await response.json();
+        //update movie ID receievd from Backend and update movie state
+        movie.id = data.name;
+        setMovies((movies)=> [...movies, movie]);
+        }
+        catch(error)
+        {
+            setError(error.message);
+        }
+        
+    },[]);
+
+    //delete movie data from backend
+    const deleteMovie = useCallback(async (title)=>{
+        let id;
+        let elementToBeDeleted;
+        console.log('movies before loop', movies);
+        for(let i=0; i<movies.length;i++)
+        {
+            if(movies[i].title === title)
+            {
+                elementToBeDeleted = i;
+                id = movies[i].id;
+                break;
+            }
+        }
+        try
+        {
+            const response = await fetch(`https://movies-app-bc878-default-rtdb.firebaseio.com/Movies/${id}.json`,
+        {
+            method : 'DELETE',
+            body: null,
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+
+        });
+        // remove deleted element from movie array and update the state
+        movies.splice(elementToBeDeleted, 1);
+        setMovies([...movies]);
+        }
+        catch(error){
+            setError(error.message);
+        }
+    });
+
     return (
         <React.Fragment>
             <section>
-                <AddMoviesForm/>
+                <AddMoviesForm addMovies = {addMovies} deleteMovie = {deleteMovie} />
             </section>
             <section>
-                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-                <button onClick={stopRetryingHanlder}>Stop Retrying</button>
+                <Button onClick={fetchMoviesHandler}>Fetch Movies</Button>
+                <Button onClick={stopRetryingHanlder}>Stop Retrying</Button>
             </section>
             {
             isLoading && <section>
--- React_module_3/movies-app/src/components/AddMoviesForm.js
+++ React_module_3/movies-app/src/components/AddMoviesForm.js
@@ -1,9 +1,12 @@
 import React,{useRef} from 'react';
 import classes from './AddMoviesForm.module.css';
-const AddMoviesForm = () => {
+import Button from './UI/Button';
+const AddMoviesForm = (props) => {
     const title = useRef();
     const opening_text = useRef();
     const Rel_date = useRef();
+
+    console.log("I am called in AddmoviesForm");
 
     const getMovieDataHandler = (e)=>{
         e.preventDefault();
@@ -12,7 +15,12 @@ const AddMoviesForm = () => {
             openingText : opening_text.current.value,
             releaseDate : Rel_date.current.value
         }
-        console.log(movieObj);
+        props.addMovies(movieObj);
+    }
+
+    const deleteMovieHandler = (e)=>{
+        e.preventDefault();
+        props.deleteMovie(title.current.value);
     }
     return (
         <form>
@@ -22,9 +30,10 @@ const AddMoviesForm = () => {
             <textarea id='opening_text' ref={opening_text}/>
             <label forhtml='Rel_date'>Release Date</label>
             <input type='text' id='Rel_date' ref={Rel_date}/>
-            <button type = 'submit' onClick={getMovieDataHandler}>Add Movie</button>
+            <Button type = 'submit' onClick={getMovieDataHandler}>Add Movie</Button>
+            <Button onClick={deleteMovieHandler}>Delete Movie</Button>
         </form>
     );
 }
 
-export default AddMoviesForm;
+export default React.memo(AddMoviesForm);
